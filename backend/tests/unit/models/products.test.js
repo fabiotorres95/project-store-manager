92504aa8c2c58b1b3dd5b5ef72a1a7c316184a63
fabiotorres95 <engfabiotorres@outlook.com>
2023-06-28 16:18:01 -0300

    initial commit

---
+++ backend/tests/unit/models/products.test.js
@@ -0,0 +1,9 @@
+// describe('Testes do path /products - CAMADA MODEL', function () {
+//   it('GET /products - Lista todos os produtos', async function () {
+    
+//   });
+
+//   it('GET /products/:id - Lista o produto com o id dado', async function () {
+    
+//   });
+// });
