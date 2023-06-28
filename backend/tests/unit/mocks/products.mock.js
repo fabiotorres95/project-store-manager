2f89b8a6d0926fd239732798290401804f2e69eb
fabiotorres95 <engfabiotorres@outlook.com>
2023-06-28 17:09:30 -0300

    created first test from models/products

---
+++ backend/tests/unit/mocks/products.mock.js
@@ -0,0 +1,26 @@
+const allProductsFromDB = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+];
+
+const allProductsMocked = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+];
+
+module.exports = {
+  allProductsFromDB,
+  allProductsMocked,
+};
--- backend/tests/unit/models/products.test.js
+++ backend/tests/unit/models/products.test.js
@@ -1,9 +1,17 @@
-// describe('Testes do path /products - CAMADA MODEL', function () {
-//   it('GET /products - Lista todos os produtos', async function () {
-    
-//   });
+const sinon = require('sinon');
+const connection = require('../../../src/models/connection');
+const mock = require('../mocks/products.mock');
 
-//   it('GET /products/:id - Lista o produto com o id dado', async function () {
+describe('Testes do path /products - CAMADA MODEL', function () {
+  it('GET /products - Lista todos os produtos', async function () {
+    sinon.stub(connection, 'execute').resolves(mock.allProductsFromDB);
+  });
+
+  it('GET /products/:id - Lista o produto com o id dado', async function () {
     
-//   });
-// });
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
