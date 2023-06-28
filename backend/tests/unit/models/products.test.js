ef63bb1f202b6eafc9d90df25646a02c19f37b43
fabiotorres95 <engfabiotorres@outlook.com>
2023-06-28 17:10:49 -0300

    actually committed the test

--- backend/tests/unit/models/products.test.js
+++ backend/tests/unit/models/products.test.js
@@ -1,10 +1,16 @@
 const sinon = require('sinon');
+const { expect } = require('chai');
 const connection = require('../../../src/models/connection');
 const mock = require('../mocks/products.mock');
 
 describe('Testes do path /products - CAMADA MODEL', function () {
   it('GET /products - Lista todos os produtos', async function () {
     sinon.stub(connection, 'execute').resolves(mock.allProductsFromDB);
+
+    const products = await productsModel.findAll();
+
+    expect(products).to.be.an('array');
+    expect(products).to.deep.equal(mock.allProductsMocked);
   });
 
   it('GET /products/:id - Lista o produto com o id dado', async function () {
