6390c3230ef9e2d6cc27c2adf4b7b8377190333a
fabiotorres95 <engfabiotorres@outlook.com>
2023-07-03 15:24:39 -0300

    POST /products middlewares work

--- backend/src/middlewares/checkName.js
+++ backend/src/middlewares/checkName.js
@@ -1,8 +1,18 @@
-const required = (_req, _res, next) => {
+const required = (req, res, next) => {
+  const newData = req.body;
+  if (!newData.name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+
   next();
 };
 
-const hasLength5 = (_req, _res, next) => {
+const hasLength5 = (req, res, next) => {
+  const newData = req.body;
+  if (newData.name.length < 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+  
   next();
 };
 
