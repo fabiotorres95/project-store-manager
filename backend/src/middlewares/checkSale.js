173a6bc167a022d52f50c864c8aee43912472f57
fabiotorres95 <engfabiotorres@outlook.com>
2023-07-04 15:16:50 -0300

    POST /sales middlewares work

--- backend/src/middlewares/checkSale.js
+++ backend/src/middlewares/checkSale.js
@@ -1,16 +1,49 @@
-const productIdRequired = (_req, _res, next) => {
+const { productsModel } = require('../models');
+
+const productIdRequired = (req, res, next) => {
+  const data = req.body;
+
+  const result = data.find((obj) => !obj.productId);
+  if (result) {
+    return res.status(400).json({ message: '"productId" is required' });
+  }
+
   next();
 };
 
-const quantityRequired = (_req, _res, next) => {
+const quantityRequired = (req, res, next) => {
+  const data = req.body;
+
+  const result = data.find((obj) => obj.quantity === undefined);
+  if (result) {
+    return res.status(400).json({ message: '"quantity" is required' });
+  }
+
   next();
 };
 
-const quantitySize = (_req, _res, next) => {
+const quantitySize = (req, res, next) => {
+  const data = req.body;
+
+  const result = data.find((obj) => obj.quantity === 0 || obj.quantity < 1);
+  if (result) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+
   next();
 };
 
-const productIdExists = (_req, _res, next) => {
+const productIdExists = async (req, res, next) => {
+  const data = req.body;
+  const products = await productsModel.findAll();
+
+  const ids = products.map((obj) => obj.id);
+
+  const result = data.find((obj) => !ids.includes(obj.id));
+  if (result) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+
   next();
 };
 
